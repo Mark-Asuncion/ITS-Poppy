72702fd0646285fec27c5713b40e96dae9524079
Mark-Asuncion <mark.asuncion.000@gmail.com>
2024-02-20 19:29:37 +0800

    test konva

--- src/main.ts
+++ src/main.ts
@@ -1,4 +1,5 @@
 import { invoke } from "@tauri-apps/api/tauri";
+import Konva from "konva";
 
 let greetInputEl: HTMLInputElement | null;
 let greetMsgEl: HTMLElement | null;
@@ -19,4 +20,41 @@ window.addEventListener("DOMContentLoaded", () => {
     e.preventDefault();
     greet();
   });
+  // first we need to create a stage
+  var stage = new Konva.Stage({
+    container: 'root',   // id of container <div>
+    width: window.innerWidth,
+    height: window.innerHeight,
+  });
+  stage.on("mousedown", (e) => {
+    if ( e.evt.button == 1 ) {
+      stage.startDrag();
+    }
+  });
+
+  // then create layer
+  var layer = new Konva.Layer();
+  var tr = new Konva.Transformer();
+
+  // create our shape
+  var circle = new Konva.Circle({
+    x: stage.width() / 2,
+    y: stage.height() / 2,
+    radius: 70,
+    fill: 'red',
+    stroke: 'black',
+    strokeWidth: 4,
+    draggable: true,
+  });
+
+  // add the shape to the layer
+  tr.nodes([circle]);
+  layer.add(tr);
+  layer.add(circle);
+
+  // add the layer to the stage
+  stage.add(layer);
+
+  // draw the image
+  layer.draw();
 });
