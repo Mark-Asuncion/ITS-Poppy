6fc3bfbf8c45e6a7d683644995bfcef46b50f078
Mark-Asuncion <mark.asuncion.000@gmail.com>
2024-03-14 17:28:09 +0800

    move themes | add active node logic

---
+++ src/themes/diagram.ts
@@ -0,0 +1,17 @@
+export const Theme = {
+    Diagram: {
+        cornerRadius: 5,
+        stroke: "#6F6F6F",
+        fill: "#F9F9F9",
+        textPadding: 10,
+        textBg: "#FFFFFF",
+        textStroke: "#6F6F6F",
+        fontSize: 16
+    },
+    Selection: {
+        fill: "#6388eb",
+        stroke: "#1A4BCC",
+        strokeWidth: 1,
+        opacity: 0.5
+    }
+};
