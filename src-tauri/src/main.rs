298860ed53d5d9fc07b45f7d65a462f112d61376
Mark-Asuncion <111512467+Mark-Asuncion@users.noreply.github.com>
2024-10-27 19:16:25 +0800

    Profiles (Initial) | Logging Lints (#19)
    * Add AppConfig::profiles
    * Logging Lints (Errs or Warns)

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -11,12 +11,15 @@ mod error;
 mod lint;
 
 use std::fs::create_dir_all;
+use config::AppConfig;
+use tauri::Manager;
 
 fn main() {
     tauri::Builder::default()
         .manage(state::GlobalState {
             work_path: Default::default(),
-            pty:       Default::default()
+            pty:       Default::default(),
+            profile:   Default::default()
         })
         .setup(|app| {
             let conf_path = app.path_resolver().app_data_dir();
@@ -31,10 +34,27 @@ fn main() {
                 }
             }
 
-            let resource_path = app.path_resolver()
-                .resolve_resource("bundle/syntaxanalyze.py")
-                .expect("failed to resolve resource");
-            dbg!(resource_path);
+            let state: tauri::State<'_, state::GlobalState> = app.state();
+            let appconfig = AppConfig::load(&app.path_resolver());
+            if let Err(e) = appconfig {
+                dbg!(e);
+                if let Err(e) = AppConfig::delete(&app.path_resolver()) {
+                    dbg!(e);
+                }
+            }
+            else {
+                let mut appconfig = appconfig.unwrap();
+                let default_profile = appconfig.get_or_set_default_profile();
+                if let Err(e) = appconfig.write(&app.path_resolver()) {
+                    dbg!(e);
+                }
+                state.set_profile(default_profile);
+            }
+
+            // let resource_path = app.path_resolver()
+            //     .resolve_resource("bundle/syntaxanalyze.py")
+            //     .expect("failed to resolve resource");
+            // dbg!(resource_path);
             Ok(())
         })
         .invoke_handler(tauri::generate_handler![
